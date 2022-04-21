972a9a64224061f9f261d66f99be2713984c7436
luismts <deivi_0326@hotmail.com>
2022-04-21 14:19:06 -0400

    Update Ticket and beneficiary entity

---
+++ src/src/api/beneficiary/routes/beneficiary.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * beneficiary router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::beneficiary.beneficiary');
