d4077f4332c21838d3256fbec478154079560b02
luismts <deivi_0326@hotmail.com>
2022-05-06 17:56:30 -0400

    Add port logic

--- config/database.js
+++ config/database.js
@@ -1,13 +1,14 @@
 module.exports = ({ env }) => 
 {
   console.log(process.env);
+  const port = process.env['DATABASE_PORT'] && parseInt(process.env['DATABASE_PORT']);
 
   return {
     connection: {
       client: 'mysql',
       connection: {
         host: process.env['DATABASE_HOST'] || '104.196.187.29',
-        port: process.env['DATABASE_PORT'] || 3306,
+        port: port || 3306,
         database: process.env['DATABASE_NAME'] || 'reportero',
         user: process.env['DATABASE_USERNAME'] || 'root',
         password: process.env['DATABASE_PASSWORD'] || 'BoezBxeFKhBk0ztk',
