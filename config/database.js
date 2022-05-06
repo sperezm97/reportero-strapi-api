abee49bad8db277e1552254931e3b44e91d1f007
luismts <deivi_0326@hotmail.com>
2022-05-06 17:03:50 -0400

    Update batabase conf file

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'reportero'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', ''),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: process.env['DATABASE_HOST'] || '127.0.0.1',
+      port: process.env['DATABASE_PORT'] || 3306,
+      database: process.env['DATABASE_NAME'] || 'reportero',
+      user: process.env['DATABASE_USERNAME'] || 'root',
+      password: process.env['DATABASE_PASSWORD'] || '',
+      ssl: process.env['DATABASE_SSL'] || false,
     },
   },
 });
