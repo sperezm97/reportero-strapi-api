964883da6fd17852ef4a63fe124d56ae3225651a
luismts <deivi_0326@hotmail.com>
2022-05-06 17:47:05 -0400

    Add console log to database.js file

--- config/database.js
+++ config/database.js
@@ -1,13 +1,18 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'mysql',
+module.exports = ({ env }) => 
+{
+  console.log(process.env);
+
+  return {
     connection: {
-      host: process.env['DATABASE_HOST'] || '127.0.0.1',
-      port: process.env['DATABASE_PORT'] || 3306,
-      database: process.env['DATABASE_NAME'] || 'reportero',
-      user: process.env['DATABASE_USERNAME'] || 'root',
-      password: process.env['DATABASE_PASSWORD'] || '',
-      ssl: process.env['DATABASE_SSL'] || false,
+      client: 'mysql',
+      connection: {
+        host: process.env['DATABASE_HOST'] || '104.196.187.29',
+        port: process.env['DATABASE_PORT'] || 3306,
+        database: process.env['DATABASE_NAME'] || 'reportero',
+        user: process.env['DATABASE_USERNAME'] || 'root',
+        password: process.env['DATABASE_PASSWORD'] || 'BoezBxeFKhBk0ztk',
+        ssl: process.env['DATABASE_SSL'] || false,
+      },
     },
-  },
-});
+  }
+}
